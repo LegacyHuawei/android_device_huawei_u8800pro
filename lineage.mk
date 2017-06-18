13a1c24274120a08966ea9efaa711f51badf48d5
Rudolf Tammekivi <rtammekivi@gmail.com>
2017-06-18 13:33:36 +0300

    u8800pro: Rebrand to LineageOS
    Change-Id: Ia157ed0039894349153fd1194e969a837c12f591

--- lineage.mk
+++ lineage.mk
@@ -22,7 +22,7 @@ TARGET_SCREEN_WIDTH := 480
 # Custom tag for unofficial builds
 TARGET_UNOFFICIAL_BUILD_ID := LegacyHuawei
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -30,7 +30,7 @@ $(call inherit-product, device/huawei/u8800pro/full_u8800pro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := u8800pro
-PRODUCT_NAME := cm_u8800pro
+PRODUCT_NAME := lineage_u8800pro
 PRODUCT_MODEL := IDEOS X5 PRO
 PRODUCT_BRAND := Huawei
 PRODUCT_MANUFACTURER := Huawei
